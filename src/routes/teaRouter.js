87a4de3f3cd8d42e5caaa5cb11f5abc0531cd935
Daria Latushkina <loreleispirit@yandex.ru>
2022-10-19 17:21:02 +0300

    database works with teapage

--- src/routes/teaRouter.js
+++ src/routes/teaRouter.js
@@ -4,10 +4,10 @@ import { Tea } from '../db/models';
 const router = Router();
 
 router.get('/:id', async (req, res) => {
-  // const tea = await Tea.findOne({ where: { id: req.params.id } });
-  const tea = {
-    title: 'tea', description: 'text here', place: 'Moscow', id: 1,
-  };
+  const tea = await Tea.findOne({ where: { id: req.params.id } });
+  // const tea = {
+  //   title: 'tea', description: 'text here', place: 'Moscow', id: 1,
+  // };
   const initState = { tea };
   res.render('Layout', initState);
 });
