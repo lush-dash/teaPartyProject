d5556bac540755a69890ba1a32e4cdc8f48ff0bf
Artyom <alotofpainhaha@mail.ru>
2022-10-19 12:58:12 +0300

    server

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+export default function App({}) {
+  return (
+    <div className="container">
+        HELLO WORLD
+      {/* <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes> */}
+    </div>
+  );
+}
