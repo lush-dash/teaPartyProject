8d0be9dc474e20bfe69a955fe942af954c0edfdc
Daria Latushkina <loreleispirit@yandex.ru>
2022-10-20 20:03:24 +0300

    tea deletion updates

--- src/components/OneTea.jsx
+++ src/components/OneTea.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
-export default function OneTea({ oneTea }) {
+export default function OneTea({ currTea, updateDeletedTeas }) {
+  const clickHandler = async () => {
+    const response = await fetch(`/api/tea/${currTea.id}`, {
+      method: 'DELETE',
+    });
+    if (response.ok) {
+      console.log(currTea);
+      updateDeletedTeas(currTea.id);
+    }
+  };
+
   return (
     <>
       <br />
       <div className="card mb-3" style={{ maxWidth: '540px' }}>
         <div className="row g-0">
           <div className="col-md-4">
-            <img src={oneTea.img} className="img-fluid rounded-start" alt="tea" />
+            <img src={currTea.img} className="img-fluid rounded-start" alt="tea" />
           </div>
           <div className="col-md-8">
             <div className="card-body">
-              <h5 className="card-title">{oneTea.title}</h5>
-              <p className="card-text">{oneTea.description}</p>
-              <p className="card-text"><small className="text-muted">{oneTea.place}</small></p>
+              <h5 className="card-title">{currTea.title}</h5>
+              <p className="card-text">{currTea.description}</p>
+              <p className="card-text"><small className="text-muted">{currTea.place}</small></p>
+              <button onClick={clickHandler} type="button" className="btn btn-danger">Удалить</button>
             </div>
           </div>
         </div>
